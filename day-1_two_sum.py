435fe33eb7ebf5192c3431b277d14aa1d7a3a8c7
rupesh-vk <kvrv09@gmail.com>
2025-08-10 11:29:16 -0500

    Update day-1_two_sum.py

--- day-1_two_sum.py
+++ day-1_two_sum.py
@@ -1,28 +1,35 @@
 """
-Problem: <Problem Title>
-Link: <LeetCode link>
-Difficulty: <Easy | Medium | Hard>
+Problem: Two Sum
+Link: https://leetcode.com/problems/two-sum/
+Difficulty: Easy 
 
 My Strategy (in my own words):
-1. <Step 1>
-2. <Step 2>
-3. <Step 3>
+1. Two sum with out ascending order, so you can use a hash map to store the indices of the numbers.
+2. Logic: Check if the complement (target - current number) exists in the hash map.
+3. If it exists, return the indices of the current number and its complement.
+4. If it does not exist, add the current number and its index to the hash map.
 
 Time Complexity:
-- Brute Force: O(...)
-- Optimized: O(...)
+- Brute Force: O(n^2) - Checking each pair of numbers.
+- Optimized: O(n) - Single pass through the list with a hash map.
 
 Space Complexity:
-O(...)
+O(n) - Space for the hash map to store indices of numbers.
 
 ---
 
 Python Solution:
 """
 
-def solve():
-    # Your code here
-    pass
+def twosum(nums, target):
+    seen_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen_map:
+            return [seen_map[complement], i] #i is the current index and seen_map[complement] is the index of the complement
+        seen_map[num] = i
+    return []  # Return an empty list if no solution is found
+
 
 if __name__ == "__main__":
-    solve()
+     print(twosum([2,7,11,15], 9))  # Output: [0,1]
